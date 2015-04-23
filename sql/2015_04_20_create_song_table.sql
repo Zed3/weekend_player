894da6eb2ec63a15b7f88e1afca920b6a8aa934a
Shai Shofet <shai@zed3.net>
2015-04-23 14:26:28 +0300

    Cosmetic changes in SQL folder

--- sql/2015_04_20_create_song_table.sql
+++ sql/2015_04_20_create_song_table.sql
@@ -55,15 +55,3 @@ FROM weekendv2_playlist
 JOIN weekendv2_users ON added_by_email = email
 JOIN weekendv2_songs ON v = video_id
 );
-
-DROP TABLE IF EXISTS `weekendv2_votes_old`;
-CREATE TABLE weekendv2_votes_old LIKE weekendv2_votes; 
-INSERT weekendv2_votes_old SELECT * FROM weekendv2_votes;
-TRUNCATE weekendv2_votes;
-
-INSERT weekendv2_votes (song_id, user_id, value) 
-SELECT weekendv2_songs.id AS song_id, weekendv2_votes_old.user_id AS user_id, value
-FROM weekendv2_votes_old 
-JOIN weekendv2_playlist ON weekendv2_votes_old.song_id = weekendv2_playlist.id
-JOIN weekendv2_songs ON weekendv2_playlist.v = weekendv2_songs.video_id
-GROUP BY video_id, weekendv2_votes_old.user_id
