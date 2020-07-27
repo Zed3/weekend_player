9492205fa21f0debfaa2f71b6d262dac1070f4df
Shai Shofet <shaishofet@gmail.com>
2020-07-27 08:26:13 +0300

    Added Google/YouTube TOS

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
+  	<div class="footer-youtube-container">
+	  <div class="footer-youtube">
+	  	<a href="https://www.youtube.com/t/terms">YouTube Terms of Service</a> | 
+	  	<a href="https://policies.google.com/privacy">Google Privacy Policy</a>  	
+	  </div>
+	</div>
   </body>
 </html>
 <?php
--- login.php
+++ login.php
@@ -49,6 +49,7 @@ $_SESSION["vitamin"] = $vitamin;
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/css/signin.css">
+    <link rel="stylesheet" href="/css/site.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   </head>
@@ -68,6 +69,4 @@ $_SESSION["vitamin"] = $vitamin;
       </form>
 
     </div>
-
-  </body>
-</html>
+<?php require "footer.php" ?>
